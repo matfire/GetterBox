999efdb7cfbdfcf85ebd6dd8504c2f93c3007e68
Matteo Gassend <matteo.gassend@epitech.eu>
2022-02-02 14:51:51 +0100

    [add] initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+    <div className='h-screen w-full flex flex-col'>
+      <Head>
+        <title>GetterBox</title>
+      </Head>
+      <main>
+        <Component {...pageProps} data-theme="dark" />
+      </main>
+      <footer>
+        &copy; {new Date().getFullYear()} GetterBox
+      </footer>
+    </div>
+  )
+
 }
 
 export default MyApp
---
+++ pages/providers/[id].js
@@ -0,0 +1,7 @@
+export default function ProvidersList() {
+    return (
+        <div>
+            WIP
+        </div>
+    )
+}
